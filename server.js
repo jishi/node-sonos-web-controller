215f8ec66a3f5d0e44a52429cad3c143d869d04f
Jimmy Shimizu <jimmy@shimizu.se>
2013-03-27 23:42:54 +0100

    first draft

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var static = require('node-static');
+var io = require('socket.io');
+var SonosDiscovery = require('sonos-discovery');
+var discovery = new SonosDiscovery();
+var port = 8080;
+
+var fileServer = new static.Server('./static');
+
+var server = http.createServer(function (req, res) {
+	
+	req.addListener('end', function () {
+        fileServer.serve(req, res);
+    });	
+});
+
+var socketServer = io.listen(server);
+
+socketServer.sockets.on('connection', function (socket) {
+	socket.emit('topology-change', discovery.getZones());
+  	socket.on('transport-state', function (data) {
+	    console.log(data);
+  });
+});
+
+discovery.on('topology-change', function (data) {
+	socketServer.sockets.emit('topology-change', data);
+});
+
+// Attach handler for socket.io
+
+server.listen(port);
+
+console.log("http server listening on port", port);
