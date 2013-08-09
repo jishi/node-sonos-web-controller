3f69e425d1a9cc8757d44abdfb03b8564f0e544d
joemccann <joseph.isaac@gmail.com>
2013-08-09 09:39:49 -0400

    cleaned up code and made the http server respond

--- server.js
+++ server.js
@@ -1,41 +1,47 @@
-var http = require('http');
-var static = require('node-static');
-var io = require('socket.io');
-var SonosDiscovery = require('sonos-discovery');
-var discovery = new SonosDiscovery();
-var port = 8080;
+var http = require('http')
+	,	static = require('node-static')
+	,	io = require('socket.io')
+	,	SonosDiscovery = require('sonos-discovery')
+	,	discovery = new SonosDiscovery()
+	,	port = process.env.PORT || 8080
+	;
 
-var fileServer = new static.Server('./static');
+// Instantiate static file server
+var fileServer = new static.Server('./public')
 
-var server = http.createServer(function (req, res) {
+// Spin up http server and listen on proper port
+var server = http.createServer(function(req, res){
 	
-	req.addListener('end', function () {
-        fileServer.serve(req, res);
-    });	
-});
+	req.addListener('end', function(){
+		fileServer.serve(req, res)
+   }).resume()
 
-var socketServer = io.listen(server);
+}).listen(port)
 
-socketServer.sockets.on('connection', function (socket) {
-	socket.emit('topology-change', discovery.getZones());
-  	socket.on('transport-state', function (data) {
+
+// Instantiate socket.io server
+var socketServer = io.listen(server)
+
+socketServer.sockets.on('connection', function (socket){
+	
+	socket.emit('topology-change', discovery.getZones())
+
+	socket.on('transport-state', function (data) {
 	    // find player based on uuid
-	    var player = discovery.getPlayerByUUID(data.uuid);
+	    var player = discovery.getPlayerByUUID(data.uuid)
 
-	    if (!player) return;
+	    if (!player) return
 
 	    // invoke action
 	    console.log(data)
-	    player[data.state]();
-  	});
-});
+
+	    player[data.state]()
+  	})
+
+})
 
 discovery.on('topology-change', function (data) {
-	socketServer.sockets.emit('topology-change', data);
-});
+	socketServer.sockets.emit('topology-change', data)
+})
 
-// Attach handler for socket.io
-
-server.listen(port);
-
-console.log("http server listening on port", port);
+console.log("HTTP Server Listening on Port", port)
